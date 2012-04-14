2e8fccbecd7b056ed1d469ca81f7685f99e2cf51
tasermonkey <github@tasermonkeys.com>
2012-04-14 10:36:45 -0400

    try to use built in hmac algo
    This doesn't work due to the system doesn't know about md4 or RipeMd160

--- PasswordMakerProForAndroid/src/org/passwordmaker/android/hashalgos/HmacHashAlgo.java
+++ PasswordMakerProForAndroid/src/org/passwordmaker/android/hashalgos/HmacHashAlgo.java
@@ -1,6 +1,12 @@
 package org.passwordmaker.android.hashalgos;
 
 import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.passwordmaker.android.HashAlgo;
 import org.passwordmaker.android.PwmHashAlgorithm.UnderliningHashAlgo;
@@ -23,7 +29,6 @@ public class HmacHashAlgo implements UnderliningHashAlgo {
 
 	@Override
 	public int blockSize() {
-		// TODO Auto-generated method stub
 		return this.underliningHash.blockSize();
 	}
 
@@ -34,39 +39,24 @@ public class HmacHashAlgo implements UnderliningHashAlgo {
 
 	@Override
 	public byte[] getHashBlob(String key, String text) {
-		byte[] keyBytes = new byte[64];
+		final String algoName = "HMAC"
+				+ underliningHash.getAlgo().getDigestName();
 		try {
-			byte[] tmp = key.getBytes("UTF8");
-			System.arraycopy(tmp, 0, keyBytes, 0, tmp.length);
+			final Mac mac = Mac.getInstance(algoName, "BC");
+			mac.init(new SecretKeySpec(key.getBytes("UTF8"), algoName));
+			mac.reset();
+			mac.update(text.getBytes("UTF8"));
+			return mac.doFinal();
+		} catch (NoSuchProviderException e) {
+			throw new RuntimeException("Invalid hash: " + algoName, e);
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("Invalid hash " + algoName, e);
+		} catch (InvalidKeyException e) {
+			throw new RuntimeException("Invalid hash " + algoName, e);
 		} catch (UnsupportedEncodingException e) {
-			throw new RuntimeException(e);
-		}
-		// Here is a bug this should compare tmp above not keyBytes as its not dependent on size... above will crap out if this condition would have held true
-		if (keyBytes.length > blockSize()) {
-			keyBytes = underliningHash.hashText(keyBytes);
+			throw new RuntimeException("Invalid hash " + algoName, e);
 		}
 
-		byte[] kIpad = new byte[64];
-		byte[] kOpad = new byte[64];
-		// XOR key with ipad and opad values
-		for (int i = 0; i < 64; i++) {
-			kIpad[i] = (byte) (keyBytes[i] ^ (byte) 0x36);
-			kOpad[i] = (byte) (keyBytes[i] ^ (byte) 0x5c);
-		}
-		byte[] innerBytes ;
-		try {
-			byte[] tmp = text.getBytes("UTF8");
-			innerBytes = new byte[tmp.length + kIpad.length];
-			System.arraycopy(kIpad, 0, innerBytes, 0, kIpad.length);
-			System.arraycopy(tmp, 0, innerBytes, kIpad.length, tmp.length);
-		} catch (UnsupportedEncodingException e) {
-			throw new RuntimeException(e);
-		}
-		byte[] innerHash = underliningHash.hashText(innerBytes);
-		innerBytes = new byte[kOpad.length + innerHash.length];
-		System.arraycopy(kOpad, 0, innerBytes, 0, kOpad.length);
-		System.arraycopy(innerHash, 0, innerBytes, kOpad.length, innerHash.length);		
-		return underliningHash.hashText(innerBytes);
 	}
 
 }
